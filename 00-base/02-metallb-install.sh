876418f22ca186114aebdcb1d83f8507d38609b9
bartvanbenthem <bartvanbenthem@hotmail.com>
2022-09-19 10:07:27 +0200

    cleanup

--- 00-base/02-metallb-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-##############################################################
-# INSTALL METALLB
-##############################################################
-kubectl apply -f 00-base/metallb/namespace.yaml
-kubectl -n metallb-system apply -f 00-base/metallb/metallb.yaml
-kubectl -n metallb-system apply -f 00-base/metallb/pool.yaml
