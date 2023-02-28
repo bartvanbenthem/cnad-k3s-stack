85032dcd3ca1a2538ba2dd5b7198e69a7706c289
bartvanbenthem <bartvanbenthem@hotmail.com>
2023-02-28 16:38:00 +0100

    upd

--- k3s-uninstall.sh
+++ k3s-uninstall.sh
@@ -3,10 +3,19 @@
 ##############################################################
 
 # on worker nodes
+sudo k3s crictl rmi --prune
 sudo /usr/local/bin/k3s-agent-uninstall.sh
 
 # on master node
+sudo k3s crictl rmi --prune
 sudo /usr/local/bin/k3s-uninstall.sh
 
 # storage cleanup on all longhorn nodes
 sudo rm -rf longhorn/storage/*
+
+##############################################################
+# DOCKER clean-up
+##############################################################
+docker image ls --format '{{.ID}}' | xargs docker image rm --force
+docker container prune
+docker volume prune
